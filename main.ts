b1aeb3a68beab5dbbe4e3010ff964f21b7c12b9d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-09-07 16:26:43 +0100

    added block comments

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+// Dispplay 16*16 pixel frog icon at X across & Y down
+// 
 function Kermit (x: number, y: number) {
     kitronik_VIEW128x64.writeImageOLED(kitronik_VIEW128x64.matrix16x16(`
         . # # # # . . . . . . # # # # .
@@ -21,6 +23,7 @@ function Kermit (x: number, y: number) {
 }
 input.onButtonPressed(Button.A, function () {
     kitronik_VIEW128x64.clear()
+    // Display all built-in Icons 1 by 1 at standard ( very small ) size in middle of screen 
     for (let index = 0; index <= list2.length - 1; index++) {
         kitronik_VIEW128x64.writeImageOLED(list2[index], 63, 31)
         kitronik_VIEW128x64.refresh()
@@ -29,6 +32,7 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.pause(500)
     kitronik_VIEW128x64.clear()
+    // Display all Built in Icons 1 by 1 in middle of screen
     for (let index = 0; index <= list2.length - 1; index++) {
         kitronik_VIEW128x64.writeImageDouble(list2[index], 31, 15)
         kitronik_VIEW128x64.refresh()
@@ -36,7 +40,9 @@ input.onButtonPressed(Button.A, function () {
         kitronik_VIEW128x64.clear()
     }
 })
+// Print into message using built in font
 function Intro () {
+    let Delay = 0
     kitronik_VIEW128x64.clear()
     kitronik_VIEW128x64.refresh()
     basic.pause(Delay)
@@ -54,6 +60,9 @@ function Intro () {
     kitronik_VIEW128x64.refresh()
     basic.pause(Delay)
 }
+// Display custon 8*8 icons 
+// Use both standard size & double size
+// 
 input.onButtonPressed(Button.B, function () {
     kitronik_VIEW128x64.clear()
     kitronik_VIEW128x64.writeImageOLED(kitronik_VIEW128x64.matrix8x8(`
@@ -188,6 +197,8 @@ input.onButtonPressed(Button.B, function () {
         `), 32, 24)
     kitronik_VIEW128x64.refresh()
 })
+// Display 4 16*16 custom Frog icons one in each corner of the screen
+// Display double sized 16*16 custom Frog icon in centre of screen 
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     kitronik_VIEW128x64.clear()
     Kermit(0, 0)
@@ -216,9 +227,12 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     kitronik_VIEW128x64.refresh()
 })
 let list2: Image[] = []
-let Delay = 0
-Delay = 256
+// Display briew introduction text
+// 
+// 5*5 built in font so hard to read
+// 
 Intro()
+// Set up list of all built in Micro:Bit Icons
 list2 = [
 kitronik_VIEW128x64.iImage(IconNames.Heart),
 images.iconImage(IconNames.SmallHeart),
